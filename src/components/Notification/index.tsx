3cde7ac012ce21c0858010264b29ee762c96e9c5
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-10 00:07:08 -0300

    feat: Finaliza componente de Notification

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,23 @@
+import { CircleCheck, Loader } from "lucide-react";
+
+interface NotificationProps {
+  type: "loading" | "success";
+  text: string;
+}
+
+export default function Notification({ text, type }: NotificationProps) {
+  return (
+    <div className="my-12 flex flex-col items-center">
+      {type === "loading" && (
+        <Loader
+          className="animate-spin  text-cyan200 duration-1000"
+          size={72}
+        />
+      )}
+
+      {type === "success" && <CircleCheck className="text-cyan200" size={72} />}
+
+      <span className="mt-5">{text}</span>
+    </div>
+  );
+}
