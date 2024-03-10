5ce3ad8cc562a95421d10582fcfc1273bf404cdd
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-10 00:07:15 -0300

    feat: Finaliza componente de Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Menu } from "lucide-react";
+
+// Assets
+import LogoImg from "@/assets/logo_parking.png";
+
+export default function Header() {
+  return (
+    <header className="bg-cyan200">
+      <div className="container flex h-16 items-center justify-between px-2">
+        <Image
+          src={LogoImg}
+          className="h-[30px]"
+          alt="Park Logotype"
+          priority
+        />
+
+        <Menu className="text-white" size={36} />
+      </div>
+    </header>
+  );
+}
