34ac765a4bc93c656025a9ae203f5ae986291c62
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-11 15:44:42 -0300

    feat: Configura font otimizada do next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
+import { Open_Sans } from "next/font/google";
+
 // Components
 import Header from "@/components/Header";
 
 // Styles
 import "@/styles/globals.css";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-opensans",
+});
 
 export const metadata = {
   title: "Parking App",
@@ -14,11 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="flex flex-col">
+    <html lang="pt" className={openSans.variable}>
+      <body className="flex min-h-screen flex-col bg-gray100 antialiased">
         <Header />
 
-        {children}
+        <main className="container flex flex-1 flex-col px-2 py-10">
+          {children}
+        </main>
       </body>
     </html>
   );
