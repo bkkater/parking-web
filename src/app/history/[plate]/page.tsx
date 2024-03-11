830da4c148541a8db390f95121413f68e0e022d0
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-11 15:49:54 -0300

    feat: Página de detalhe da placa

---
+++ src/app/history/[plate]/page.tsx
@@ -0,0 +1,12 @@
+// Components
+import Box from "@/components/Box";
+
+export default function historyDetail() {
+  return (
+    <>
+      <Box>
+        <Box.Header />
+      </Box>
+    </>
+  );
+}
