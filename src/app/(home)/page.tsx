a50c632de93eb231b26727801042442848ab8f10
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-13 11:53:04 -0300

    feat: Integração do Front-end

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,47 +1,60 @@
-import Link from "next/link";
+import { z } from "zod";
 
 // Components
-import Button from "@/components/Button";
-import Form from "@/components/Form";
+import EntryForm, { EntryFormSchema } from "@/app/(home)/entryForm";
+import ExitForm, { ExitFormSchema } from "@/app/(home)/exitForm";
 import HomeTabs from "@/app/(home)/tabs";
 import HomeTab from "@/app/(home)/tab";
 
+// Utils
+import { plateRegex } from "@/utils/regex";
+
+// Data
+import { api } from "@/data/api";
+
 export default function Home() {
+  async function handleSubmitEntry(data: EntryFormSchema) {
+    "use server";
+
+    await api("/", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  }
+
+  async function handleSubmitExit(data: ExitFormSchema) {
+    "use server";
+
+    const { plate } = data;
+
+    await api(`/${plate}/out`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  }
+
+  async function handleSubmitPaymennt(data: ExitFormSchema) {
+    "use server";
+
+    const { plate } = data;
+
+    await api(`/${plate}/pay`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  }
+
   return (
     <HomeTabs>
       <HomeTab value="entry">
-        <Form>
-          <Form.Field>
-            <Form.Label htmlFor="plate">Numero da placa:</Form.Label>
-
-            <Form.Input id="plate" type="text" placeholder="AAA-0000" />
-          </Form.Field>
-
-          <Button>Confirmar entrada</Button>
-        </Form>
+        <EntryForm onSubmitEntry={handleSubmitEntry} />
       </HomeTab>
 
       <HomeTab value="exit">
-        <Form>
-          <Form.Field>
-            <Form.Label htmlFor="plate">Numero da placa:</Form.Label>
-
-            <Form.Input id="plate" type="text" placeholder="AAA-0000" />
-          </Form.Field>
-
-          <Button color="secoundary">Pagamento</Button>
-
-          <Button color="secoundary" variant="outlined">
-            Saída
-          </Button>
-
-          <Link
-            href="/history"
-            className="mx-auto mt-3 font-semibold uppercase text-cyan200"
-          >
-            Ver histórico
-          </Link>
-        </Form>
+        <ExitForm
+          onSubmitPayment={handleSubmitPaymennt}
+          onSubmitExit={handleSubmitExit}
+        />
       </HomeTab>
     </HomeTabs>
   );
