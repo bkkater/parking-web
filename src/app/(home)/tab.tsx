c8e02c8aa960c2fa4ade7484a08651ddf0f05981
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-13 11:55:13 -0300

    feat: Organização do código + Componente de Error

--- src/app/(home)/tab.tsx
+++ src/app/(home)/tab.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// Components
 import Tabs from "@/components/Tabs";
 
 export default function HomeTab({
---
+++ src/components/Error/index.tsx
@@ -0,0 +1,15 @@
+import { CircleAlert } from "lucide-react";
+
+type ErrorProps = {
+  text?: string;
+};
+
+export default function Error({ text }: ErrorProps) {
+  return (
+    <div className="flex h-8 w-full animate-fadeIn items-center gap-2 rounded bg-pink100 px-3 text-sm font-semibold text-pink700">
+      <CircleAlert className="text-pink700" size={20} />
+
+      {text}
+    </div>
+  );
+}
