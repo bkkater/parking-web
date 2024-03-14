c30651ccfe09b0735e20431ad6d4cedb4fd04e32
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-14 11:37:19 -0300

    feat: Novo componente de Menu

--- src/app/(home)/tabs.tsx
+++ src/app/(home)/tabs.tsx
@@ -1,11 +1,48 @@
 "use client";
+import { useEffect, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
 // Components
 import Tabs from "@/components/Tabs";
 
-export default function HomeTabs({ children }: { children: React.ReactNode }) {
+type TabValue = "entry" | "exit";
+
+type HomeTabsProps = {
+  children: React.ReactNode;
+};
+
+const DEFAULT_TAB = "entry";
+
+export default function HomeTabs({ children }: HomeTabsProps) {
+  const searchParams = useSearchParams();
+
+  const initialTab = searchParams.get("tab") as TabValue;
+  const [activeTab, setActiveTab] = useState<TabValue>(
+    initialTab || DEFAULT_TAB,
+  );
+
+  const handleTabChange = (value: string) => {
+    const tabValue = value as TabValue;
+
+    if (tabValue) {
+      setActiveTab(tabValue);
+    }
+  };
+
+  useEffect(() => {
+    const value = searchParams.get("tab");
+
+    if (value === "entry" || value === "exit") {
+      setActiveTab(value);
+    }
+  }, [searchParams]);
+
   return (
-    <Tabs defaultValue="entry">
+    <Tabs
+      defaultValue="entry"
+      value={activeTab || DEFAULT_TAB}
+      onValueChange={handleTabChange}
+    >
       <Tabs.List className="flex" aria-label="Registre os carros estacionados">
         <Tabs.Trigger value="entry">Entrada</Tabs.Trigger>
 
