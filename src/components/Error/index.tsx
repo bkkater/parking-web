f6b966b2c686f77ff409576cb469b229ce277510
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-15 19:02:53 -0300

    style: Corrige responsividade do error

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -7,7 +7,7 @@ type ErrorProps = {
 export default function Error({ text }: ErrorProps) {
   return (
     <div
-      className="flex h-8 w-full animate-fadeIn items-center gap-2 rounded bg-pink100 px-3 text-sm font-semibold text-pink700"
+      className="flex h-min w-full animate-fadeIn items-center gap-2 rounded bg-pink100 px-2 py-1 text-sm font-semibold text-pink700"
       data-testid="state_message"
     >
       <CircleAlert className="text-pink700" size={20} />
