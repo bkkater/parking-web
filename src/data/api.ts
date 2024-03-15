927df8983d060f02e596cb7504adbd331f1a92d5
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-15 18:59:56 -0300

    feat: Correção do .env

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,8 +1,9 @@
-const BASE_URL = "https://parking-lot-to-pfz.herokuapp.com";
-
 export function api(path: string, init?: RequestInit) {
   const apiPrefix = "/parking";
 
-  const url = new URL(apiPrefix.concat(path), BASE_URL);
+  const url = new URL(
+    apiPrefix.concat(path),
+    process.env.NEXT_PUBLIC_API_BASE_URL,
+  );
   return fetch(url, init);
 }
