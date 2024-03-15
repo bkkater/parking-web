0efa14bad8f6c664cb384c4b1815158d5465fc98
Bianca Medeiros <bianca.medeiros@dotlib.com>
2024-03-15 20:38:00 -0300

    feat: Altera base url do cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://parking-webapp.vercel.app",
     setupNodeEvents(on, config) {},
   },
 });
